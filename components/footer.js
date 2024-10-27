bb7e97f96fe6bcb72904255bb0b2b21daf7d4b98
Kushagra Pandey <kushagra.15554@gmail.com>
2024-10-27 22:02:35 +0530

    Changed background color of footer

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
   return (
     <>
       <div className="flex justify-center mt-[100px] pb-[50px] animate-fade-slide-in-top">
-        <div className="relative border bg-[#FFB38E] border-gray-400 rounded-3xl p-3 flex justify-center items-center w-fit overflow-hidden">
+        <div className="relative border bg-[#71C9CE] border-gray-400 rounded-3xl p-3 flex justify-center items-center w-fit overflow-hidden">
           <div className="flex flex-row items-center h-[50px] space-x-2">
             <Link href="https://github.com/thundergaming761">
               <Image
